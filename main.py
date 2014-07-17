f6ce0b379b59e64bb75bd06b64e82cb075c7a199
Thomas Coppeans <teccoppeans@gmail.com>
2014-07-17 05:25:36 -0400

    Main game logic

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 import pygame, sys
 from pygame.locals import *
+
+
+def main():
+    init()
+    while True:
+        update()
+        draw()
+    
+
+
+def update():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+def draw():
+    pygame.display.update()
+
+
+def init():
+    pygame.init()
+    size = width, height = 400, 300
+    screen = pygame.display.set_mode(size)
+    
+if __name__ == "__main__":
+    main()
